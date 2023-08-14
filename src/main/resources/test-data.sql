ba6c7af1d39d095fad7661fc324196f22bdefc25
박혜민/백엔드개발팀 <hyemin@ktown4u.com>
2023-08-14 20:44:56 +0900

    Add test data

---
+++ src/main/resources/test-data.sql
@@ -0,0 +1,11 @@
+insert into ORDERS (ID, USER_ID)
+values (1, 1),
+       (2, 1),
+       (3, 1);
+
+insert into ORDERED_PRODUCTS (ID, NAME, PRICE, ORDER_ID)
+values (1, 'product 1', 1000, 1),
+       (2, 'product 2', 900, 1),
+       (3, 'product 3', 1500, 2),
+       (4, 'product 4', 2000, 3),
+       (5, 'product 1', 1000, 3);
