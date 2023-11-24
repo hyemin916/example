e9ec11989c35f41f1f2426d04d2532c6e947fa95
박혜민/백엔드개발팀 <hyemin@ktown4u.com>
2023-11-24 21:04:29 +0900

    create product entity

---
+++ src/main/java/com/java/example/entities/Product.java
@@ -0,0 +1,24 @@
+package com.java.example.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Table(name = "PRODUCT")
+@Entity
+public class Product {
+
+    @Id
+    @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "NAME")
+    private String name;
+
+    @Column(name = "BARCODE")
+    private String barcode;
+}
