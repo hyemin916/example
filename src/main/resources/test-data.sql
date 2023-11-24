ea0f8058c0623f7c51cb5cd56aff2d51cafb88ca
박혜민/백엔드개발팀 <hyemin@ktown4u.com>
2023-11-24 21:08:15 +0900

    add querydsl

--- src/main/resources/test-data.sql
+++ src/main/resources/test-data.sql
@@ -13,4 +13,9 @@ values (1, 'product 1', 1000, 1),
 insert into SHIPPINGS (ID, ADDRESS, ORDER_ID)
 values (1, 'address 1', 1),
        (2, 'address 2', 2),
-       (3, 'address 3', 3)
+       (3, 'address 3', 3);
+
+insert into PRODUCT (ID, NAME, BARCODE)
+values (1, 'TAEMIN - The 4th Mini Album [Guilty] (Photo Book Ver.) (GUILTY Ver.)', '8804775367427'),
+       (2, 'LUXURY 2023.10 (Cover : SHINee : TAEMIN)', '9000000100557'),
+       (3, '6v6 베개커버_SPMAB49U22 3', '9000000062610');
